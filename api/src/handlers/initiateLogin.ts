5d520ef707af9ba30dc679b8e8f46ccbd93a6f98
dickhardt <dick.hardt@hello.coop>
2024-10-28 09:01:20 +0000

    domain_hint support

--- api/src/handlers/initiateLogin.ts
+++ api/src/handlers/initiateLogin.ts
@@ -8,19 +8,24 @@ import config from '../lib/config'
 type InitiateLoginParams = {
     iss: string,
     login_hint: string,
+    domain_hint: string,
     target_link_uri: string,
 }
 
 const initiateLogin = async (req: HelloRequest, res: HelloResponse, params: InitiateLoginParams  ) => {
-    const { iss, login_hint, target_link_uri } = params
+    const { iss, login_hint, domain_hint, target_link_uri } = params
 
     const issuer = `https://issuer.${config.helloDomain}`
     if (iss !== issuer) {
         return res.send(`Passed iss '${iss}' must be '${issuer}'`)
     }
     req.query = {
-        login_hint,
         target_uri: target_link_uri,
+    }
+    if (login_hint) {
+        req.query.login_hint = login_hint
+    } else if (domain_hint) {
+        req.query.domain_hint = domain_hint
     }
     return handleLogin(req, res)
 }
