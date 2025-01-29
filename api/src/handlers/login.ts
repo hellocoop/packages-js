fcdd92c18c4583bc64c3356babf3a63109ff9207
dickhardt <dick.hardt@hello.coop>
2025-01-29 16:28:55 +0000

    remove redirect_uri cache, bounce everytime if not configured

--- api/src/handlers/login.ts
+++ api/src/handlers/login.ts
@@ -5,9 +5,6 @@ import { Scope, ProviderHint } from '@hellocoop/definitions'
 import config from '../lib/config'
 import { saveOidc } from '../lib/oidc'
 
-export const redirectURIs: Record<string, any> = {}
-
-// var callCount = 0 // DEBUG
 
 const handleLogin = async (req: HelloRequest, res: HelloResponse) => {
     const { provider_hint: providerParam, scope: scopeParam, target_uri, redirect_uri, nonce: providedNonce, prompt, login_hint, domain_hint } = req.query
@@ -18,33 +15,10 @@ const handleLogin = async (req: HelloRequest, res: HelloResponse) => {
         return
     }
 
-// callCount++
-// console.log('login called:',callCount)
-
-    let redirectURI = config.redirectURI as string
-    let host = req.headers()?.host as string
+    let redirectURI = config.redirectURI || redirect_uri as string
     if (!redirectURI) {
-        if (redirectURIs[host]) {
-            redirectURI = redirectURIs[host]
-        } else {
-            if (redirect_uri) {
-//                 const redirectUriString =  (Array.isArray(redirect_uri) ? redirect_uri[0] : redirect_uri) as string
-//                 const redirectHost = (new URL(redirectUriString)).host
-//                 if (redirectHost != host) {
-// // TBd -- this might happen if we are behind a proxy where our host and the browser host are different -- look at X-headerrs
-//                     const err = `host from redirect_uri=${redirectHost}, expected ${host}`
-//                     console.error(err)
-//                     res.status(500)
-//                     res.send(err)
-//                     return 
-//                 }
-                redirectURIs[host] = redirectURI = redirect_uri as string
-                console.log(`Hellō: RedirectURI for ${host} => ${redirectURI}`)
-            } else {            
-                console.log('Hellō: Discovering API RedirectURI route ...')
-                return res.send(redirectURIBounce())        
-            }
-        }
+        console.log('Hellō: Discovering API RedirectURI route ...')
+        return res.send(redirectURIBounce())        
     }
     // parse out param strings
     const targetURIstring = (Array.isArray(providerParam) ? providerParam[0] : providerParam) as string
