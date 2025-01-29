b6213afae0a9905a74a795c30fb9cb2ee562bec1
dickhardt <dick.hardt@hello.coop>
2025-01-29 16:14:14 +0000

    fix redirect_uri

--- api/src/handlers/initiateLogin.ts
+++ api/src/handlers/initiateLogin.ts
@@ -6,25 +6,30 @@ import handleLogin from './login'
 import config from '../lib/config'
 
 type InitiateLoginParams = {
-    iss: string,
-    login_hint: string,
-    domain_hint: string,
-    target_link_uri: string,
+    iss?: string,
+    login_hint?: string,
+    domain_hint?: string,
+    target_link_uri?: string,
+    redirect_uri?: string
 }
 
 const initiateLogin = async (req: HelloRequest, res: HelloResponse, params: InitiateLoginParams  ) => {
-    const { iss, login_hint, domain_hint, target_link_uri } = params
+    const { iss, login_hint, domain_hint, target_link_uri, redirect_uri } = params
 
     const issuer = `https://issuer.${config.helloDomain}`
     if (iss && (iss !== issuer)) {
         return res.send(`Passed iss '${iss}' must be '${issuer}'`)
     }
-    req.query = { // override query params
-        target_uri: target_link_uri,
+    req.query = {} // override query params
+    if (target_link_uri) {
+        req.query.target_uri = target_link_uri
+    }
+    if (redirect_uri) {
+        req.query.redirect_uri = redirect_uri
     }
     if (login_hint) {
         req.query.login_hint = login_hint
-    } else if (domain_hint) {
+    } else if (domain_hint) { // if both are passed, login_hint takes precedence
         req.query.domain_hint = domain_hint
     }
     return handleLogin(req, res)
