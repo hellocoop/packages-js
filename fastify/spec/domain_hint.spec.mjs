3c5f207018b2013f63aba71241b15aab13602084
rohanharikr <rohanharikumar80@gmail.com>
2025-02-05 14:24:00 +0000

    update fastify hint values for consistency

--- fastify/spec/domain_hint.spec.mjs
+++ fastify/spec/domain_hint.spec.mjs
@@ -7,7 +7,7 @@ const config = {
     client_id: '8c3a40a9-b235-4029-8d16-c70592ca94bb',
 }
 
-const domainHint = 'rsandh.com'
+const domainHint = 'example.org'
 
 describe('domain_hint', () => {
     let fastify = null
--- fastify/spec/login_hint.spec.mjs
+++ fastify/spec/login_hint.spec.mjs
@@ -7,7 +7,7 @@ const config = {
     client_id: '8c3a40a9-b235-4029-8d16-c70592ca94bb',
 }
 
-const loginHint = 'johnsmith@me.com'
+const loginHint = 'dan.brown@example.net'
 
 describe('login_hint', () => {
     let fastify = null
