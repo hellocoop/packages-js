dcb50d2042b1387b398fdc416afda78cbe7a86cc
dickhardt <dick.hardt@hello.coop>
2025-02-06 17:35:11 +0000

    nicer layout

--- api/src/handlers/invite.ts
+++ api/src/handlers/invite.ts
@@ -13,11 +13,16 @@ const handleInvite = async (req: HelloRequest, res: HelloResponse) => {
         req.query
 
     const auth = await getAuthfromCookies(req, res)
-    if (!auth.isLoggedIn) return res.status(401).send('Not logged in')
-    if (!auth.sub) return res.status(401).send('Missing sub in auth')
-    if (!auth.email) return res.status(401).send('Missing email in auth')
-    if (!auth.name) return res.status(401).send('Missing name in auth')
-    if (!app_name) return res.status(400).send('Missing app_name')
+    if (!auth.isLoggedIn)
+        return res.status(401).send('Not logged in')
+    if (!auth.sub)
+        return res.status(401).send('Missing sub in auth')
+    if (!auth.email)
+        return res.status(401).send('Missing email in auth')
+    if (!auth.name)
+        return res.status(401).send('Missing name in auth')
+    if (!app_name)
+        return res.status(400).send('Missing app_name')
 
     const redirectURI = config.redirectURI || (redirect_uri as string)
     if (!redirectURI) {
